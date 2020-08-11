b7f71715922c4ebdfefa966cbae83537fca1abfa
buddi56 <ynaveenk.0513@gmail.com>
2020-08-11 22:15:03 +0530

    Ignore apex updates: it has issues like no access to Internal Storage (seen in few ROMs)

--- aosp.mk
+++ aosp.mk
@@ -14,9 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
